b68e763bba4de66dcc2cd0bb67287767f92a9cfc
vishnupriyan <priyanvishnu15@gmail.com>
2025-06-16 18:42:53 +0530

    updates the env import

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IExepnse, IExpenseType, IIncome, IIncomeType } from '../types/types';
-import { environment } from '../../environments/environment.development';
+import { environment } from '../../environments/environment';
 
 @Injectable({
     providedIn: 'root'
